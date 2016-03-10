a34a76e469bea8d079c1978e7e637b9add9e50c2
ArkadiyODS <arkadiy.andriyenko@gmail.com>
2016-03-10 22:08:40 +0200

    Added files via upload

---
+++ HW_BitOperation.java
@@ -0,0 +1,25 @@
+/*
+ 9. Есть переменная типа int (32 бита), равная нулю. Присвоить трём случайным 
+    битам значение 1. Показать получившееся значение переменной.
+ */
+package hw_bitoperation;
+
+public class HW_BitOperation {
+
+    public static void main(String[] args) {
+        int  x = 0;    
+        char[] array = String.format("%32s", Integer.toBinaryString(x)).replace(' ', '0').toCharArray();
+        
+        System.out.println(String.valueOf(array));    
+        for (int i = 0; i < 3; i++) {
+            array[(int)(Math.random()*32)]='1';
+        }    
+        System.out.println(String.valueOf(array));
+        System.out.println(Integer.parseInt(String.valueOf(array), 2));
+    }  
+}
+/*
+00000000000000000000000000000000
+00000000000100100000000000010000
+1179664
+*/
